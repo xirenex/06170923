682135fe0dd98a881a6a5486dd9164e6cbca2340
xirenex <62133453+xirenex@users.noreply.github.com>
2020-05-15 14:12:33 +0800

    Update views.py

--- studentsapp/views.py
+++ studentsapp/views.py
@@ -71,7 +71,7 @@ def hello3 (request,username):
 def index(request):
                 now=datetime.now()
                 username="daphne lo" 
-                return render(request,"06170923.html",locals())
+                return render(request,"06170923(15).html",locals())
 
 def listone(request): 
 	try: 
